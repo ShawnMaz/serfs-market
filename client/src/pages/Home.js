d37ab9db2aa921710642b6817a5ee284918d5a1e
raposamillar <lisa.raposamillar@gmail.com>
2022-12-02 13:01:09 -0500

    set shell for Home variable

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,1 +1,17 @@
 import React from 'react';
+
+// import Auth from '../utils/auth';
+
+const Home = () => {
+
+// const loggedIn = Auth.loggedIn();
+
+  return (
+    <main>
+
+    </main>
+  )  
+
+};
+
+export default Home;
