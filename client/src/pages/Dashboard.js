689adfa4af9680354fea392826c438a53050da02
Tmeng <tonymengt@gmail.com>
2022-12-03 16:51:18 -0500

    added reducers and action in util and test

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import Auth from '../utils/auth';
+import News from '../components/News';
 
 const Dashboard = () => {
+
+
+    
     return (
-        <div>
+        <section>
+            <div>
+                Logo
+            </div>
+            <div>
+                <h2>
+                    My Gains
+                </h2>
+
+                <div>
+                    data on current stock price
+                </div>
+            </div>
+            
+            <div>
+                <h2>
+                    My Stocks
+                </h2>
+
+                <div>
+                    my portfolio
+                </div>
+            </div>
+            <div>
+                <h2>
+                    Serf’s News
+                </h2>
+                <News />
+            </div>
+
             test
-        </div>
+        </section>
     )
 }
 
---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,10 @@
+// import React, {createContext, useContext} from 'react'
+
+// const StockContext = createContext();
+// const { Provider } = StockContext;
+
+// const StockProvider = ({ value, props }) => {
+//     const [state, dispatch] = useStockReducer({
+//         stocks: 
+//     })
+// }
