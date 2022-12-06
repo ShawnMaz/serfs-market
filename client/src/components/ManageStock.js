1f86d76647aa595bee8760d303fa793b01b709f4
Tmeng <tonymengt@gmail.com>
2022-12-05 19:32:23 -0500

    updated the managed stock component

--- client/src/components/ManageStock.js
+++ client/src/components/ManageStock.js
@@ -1,42 +1,75 @@
 import React, {useState} from "react";
 import { useMutation, useQuery } from "@apollo/client";
-import { QUERY_STOCKS } from "../utils/queries";
+import { QUERY_STOCKS, QUERY_USER } from "../utils/queries";
 import { BUY_STOCK, SELL_STOCK } from "../utils/mutations";
 
-const ManageStocks= () => {
+const ManageStocks= ({stock}) => {
 
-    const { loading, data } = useQuery(QUERY_STOCKS)
-    const allStocks = data?.stocks || [];
+    const [ quantity, setQuantity ] = useState(1)
+    console.log(stock)
 
+    // , {
+    //   refetchQueries: [{query:QUERY_USER}]
+    // }
     const [ purchaseStock ] = useMutation(BUY_STOCK)
-    const handleClick = async () => {
+    const buyClick = async (e) => {
+      const id = e.target.dataset.id
+      console.log(e.target.dataset.id)
         try {
           await purchaseStock({
-            variables: { id: allStocks._id }
-          });
+            variables: { stockId: id, qty: parseInt(quantity) }
+          }); 
+
         } catch (e) {
           console.log(e);
         }
       }
-    if(loading) {
-        <h2>LOADING...</h2>
-    }
+
+
+    const [ sellStock ] = useMutation(SELL_STOCK)
+    const sellClick = async (e) => {
+      const id = e.target.dataset.id
+      console.log(e.target.dataset.id)
+        try {
+          await sellStock({
+            variables: { stockId: id, qty: parseInt(quantity) }
+          }); 
+
+        } catch (e) {
+          console.log(e);
+        }
+      }
+
 return (
     <section>
-            <h2>
+            {/* <h2>
                 Current Stocks
             </h2>
 
             <ul>
                 {allStocks.map((stock) => (
-                    <li key={stock._id}>
-                    {stock.name}
-                    {stock.price}
-                    {stock.description}
-                    <button onClick={handleClick}>Buy</button>
+                    <li key={stock._id} >
+                    {stock.stockName}
+                    {stock.stockPrice}
+                    {stock.stockDescription}
+                    <input value={quantity} onChange={(e)=> setQuantity(e.target.value)}/>
+                    <button onClick={handleClick} data-id={stock._id}>Buy</button>
                     </li>
                 ))}
-            </ul>
+            </ul> */}
+
+        <div>{stock.stockCategory}, {stock.stockName}, ${stock.stockPrice}, {stock.stockDescription}</div>
+        <div>
+          <span>Qty:</span>
+          <input
+            type="number"
+            placeholder=""
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+          />
+          <button onClick={buyClick} data-id={stock._id}>Buy</button>
+          <button onClick={sellClick} data-id={stock._id}>Sell</button>
+        </div>
     </section>
 )
 }
