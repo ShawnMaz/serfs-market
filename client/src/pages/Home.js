36cbbc06253305d3bd84da507e57f3795af47ae3
raposamillar <lisa.raposamillar@gmail.com>
2022-12-05 22:00:23 -0500

    paste missing Home.js code from feature/client

---
+++ client/src/pages/Home.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+// import Login from '../pages/Login';
+import Signup from '../components/Signup';
+import market from '../assets/images/market.jpg';
+import { QUERY_STOCKS } from '../utils/queries';
+
+
+const Home = () => {
+  const { loading, data } = useQuery(QUERY_STOCKS);
+  const stockData = data?.stocks || [];
+
+  console.log(stockData)
+  if (loading) {
+    return <h2>LOADING...</h2>
+  }
+
+
+  return (
+    <main className='home'>
+      <h2>
+        The Serf’s Market - A Medieval Stock Exchange
+      </h2>
+      <span role='img' aria-label='market'>
+        <img src={market} style={{ width: '50%'}} alt='Peasants at the market.' />
+      </span>
+      <div>
+        Stock Status
+        <ul>
+          {stockData.map((stocks) => (
+            <li key={stocks._id}>
+              {stocks.stockName}<br/>
+              {stocks.stockPrice * stocks.multiplier} <br/>
+              {stocks.stockDescription}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <Signup />
+      </div>
+    </main>
+  )  
+
+};
+
+export default Home;
